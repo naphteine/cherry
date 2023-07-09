d75d6412099374eca4c817da449b1ddc4dfd246f
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-07-09 16:43:43 +0300

    Kinda working

--- js/tomato.js
+++ js/tomato.js
@@ -1,128 +1,106 @@
 (function () {
   "use strict";
 
+  let elTomato = document.getElementById("tomato");
   let elStatus = document.getElementById("status");
   let elButton = document.getElementById("button");
+  let elTime = document.getElementById("time");
 
   let status = "Stopped";
+  let lastStatus = "tomato";
+  let tomatoCounter = 0;
+  let tomatoMinute = 25;
+  let breakCounter = 0;
+  let breakMinute = 5;
+
+  let timer = 0;
+  let timerMinute = 0;
+  let color = "red";
+
+  let interval = 1000;
+  let expected = Date.now() + interval;
 
   elButton.addEventListener("click", (event) => {
-    status = status == "Started" ? "Stopped" : "Started";
-    elStatus.innerText = status;
+    if (status == "Stopped") {
+      changeStatus(lastStatus);
+    } else {
+      lastStatus = status;
+      changeStatus("Stopped");
+    }
 
+    elStatus.innerText = status;
     elButton.innerText = elButton.innerText == "Start" ? "Stop" : "Start";
   });
 
-  // let canvas = document.getElementById("mainCanvas");
-  // let ctx = canvas.getContext("2d");
+  const changeStatus = function (newStatus) {
+    status = newStatus;
 
-  // let status = "stopped";
-  // let lastStatus = "tomato";
-  // let tomatoCounter = 0;
-  // let tomatoMinute = 25;
-  // let breakCounter = 0;
-  // let breakMinute = 5;
+    switch (newStatus) {
+      case "tomato":
+        color = "blue";
+        break;
+      case "break":
+        color = "green";
+        break;
+      default:
+        color = "red";
+        break;
+    }
 
-  // let timer = 0;
-  // let timerMinute = 0;
-  // let color = "red";
+    elTomato.style.backgroundColor = color;
+  };
 
-  // let interval = 1000;
-  // let expected = Date.now() + interval;
+  setInterval(function () {
+    let statusText, timerText;
 
-  // const changeStatus = function(newStatus) {
-  // 	status = newStatus;
+    if (status == "break") {
+      statusText = status;
+      timerText = timerMinute + ":" + timer + "/" + breakMinute + ":00";
+    } else if (status == "tomato") {
+      statusText = status;
+      timerText = timerMinute + ":" + timer + "/" + tomatoMinute + ":00";
+    } else {
+      statusText = lastStatus + " " + status;
 
-  // 	switch (newStatus) {
-  // 		case "tomato":
-  // 			color = "blue";
-  // 			break;
-  // 		case "break":
-  // 			color = "green";
-  // 			break;
-  // 		default:
-  // 			color = "red";
-  // 			break;
-  // 	}
-  // };
+      if (lastStatus == "break") {
+        timerText = timerMinute + ":" + timer + "/" + breakMinute + ":00";
+      } else {
+        timerText = timerMinute + ":" + timer + "/" + tomatoMinute + ":00";
+      }
+    }
 
-  // canvas.addEventListener("click", function() {
-  // 	if (status == "stopped") {
-  // 		changeStatus(lastStatus);
-  // 	} else {
-  // 		lastStatus = status;
-  // 		changeStatus("stopped");
-  // 	}
-  // })
+    document.title = timerText + " - " + statusText;
+  }, 10);
 
-  // setInterval(function() {
-  // 	canvas.width = window.innerWidth;
-  // 	canvas.height = window.innerHeight;
+  const step = function () {
+    let dt = Date.now() - expected;
+    if (dt > interval) {
+      // SNAFU
+    }
 
-  // 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // 	ctx.fillStyle = color;
-  // 	ctx.beginPath();
-  // 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-  // 	ctx.stroke();
-  // 	ctx.font = "24px serif";
-  // 	ctx.textBaseline = "middle";
-  // 	ctx.textAlign = "center";
+    if (status != "Stopped") {
+      timer++;
 
-  // 	let statusText, timerText;
+      if (timer > 59) {
+        if (status == "tomato" && timerMinute + 1 >= tomatoMinute) {
+          tomatoCounter++;
+          timerMinute = 0;
+          changeStatus("break");
+        } else if (status == "break" && timerMinute + 1 >= breakMinute) {
+          breakCounter++;
+          timerMinute = 0;
+          changeStatus("tomato");
+        } else {
+          timerMinute++;
+        }
 
-  // 	if (status == "break") {
-  // 		statusText = status;
-  // 		timerText = timerMinute + ":" + timer + "/" + breakMinute + ":00";
-  // 	} else if (status == "tomato") {
-  // 		statusText = status;
-  // 		timerText = timerMinute + ":" + timer + "/" + tomatoMinute + ":00";
-  // 	} else {
-  // 		statusText = lastStatus + " " + status;
+        timer = 0;
+      }
+    }
 
-  // 		if (lastStatus == "break") {
-  // 			timerText = timerMinute + ":" + timer + "/" + breakMinute + ":00";
-  // 		} else {
-  // 			timerText = timerMinute + ":" + timer + "/" + tomatoMinute + ":00";
-  // 		}
-  // 	}
+    expected += interval;
+    setTimeout(step, Math.max(0, interval - dt));
+  };
 
-  // 	ctx.fillStyle = "white";
-  // 	ctx.fillText(statusText, canvas.width / 2, canvas.height / 2);
-  // 	ctx.fillText(timerText, canvas.width / 2, canvas.height / 2 + 25);
-  // 	ctx.fillText("tomatos: " + tomatoCounter + " / breaks: " + breakCounter, canvas.width / 2, canvas.height / 2 + 50);
-
-  // 	document.title = timerText + " - " + statusText;
-  // }, 10)
-
-  // const step = function() {
-  // 	let dt = Date.now() - expected;
-  // 	if (dt > interval) {
-  // 		// SNAFU
-  // 	}
-
-  // 	if (status != "stopped") {
-  // 		timer++;
-
-  // 		if (timer > 59) {
-  // 			if (status == "tomato" && timerMinute + 1 >= tomatoMinute) {
-  // 				tomatoCounter++;
-  // 				timerMinute = 0;
-  // 				changeStatus("break");
-  // 			} else if (status == "break" && timerMinute + 1 >= breakMinute) {
-  // 				breakCounter++;
-  // 				timerMinute = 0;
-  // 				changeStatus("tomato");
-  // 			} else {
-  // 				timerMinute++;
-  // 			}
-
-  // 			timer = 0;
-  // 		}
-  // 	}
-
-  // 	expected += interval;
-  // 	setTimeout(step, Math.max(0, interval - dt));
-  // };
-
-  // setTimeout(step, interval);
+  setTimeout(step, interval);
 })();
